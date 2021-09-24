abb39ad8cc4cd169c7e4cc9b95b8a37383f00714
murarireddypaluru <76266934+murarireddypaluru@users.noreply.github.com>
2021-09-24 07:44:56 -0700

    Add files via upload

---
+++ RemoveFiles.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+import time
+
+
+def main():
+	path = "/Any/Path/Name"
+	days = 30
+	seconds = time.time() - (days * 24 * 60 * 60)
+	if os.path.exists(path):
+		for root_folder, folders, files in os.walk(path):
+			if seconds >= get_file_or_folder_age(root_folder):
+				remove_folder(root_folder)
+				break
+			else:
+				for folder in folders:
+					folder_path = os.path.join(root_folder, folder)
+					if seconds >= get_file_or_folder_age(folder_path):
+						remove_folder(folder_path)
+				for file in files:
+					file_path = os.path.join(root_folder, file)
+					if seconds >= get_file_or_folder_age(file_path):
+						remove_file(file_path)
+		else:
+			if seconds >= get_file_or_folder_age(path):
+				remove_file(path)
+
+	else:
+		print(f'"{path}" is not found')
+
+
+
+def remove_folder(path):
+
+	# removing the folder
+	if not shutil.rmtree(path):
+
+		# success message
+		print(f"{path} is removed successfully")
+
+	else:
+
+		# failure message
+		print(f"Unable to delete the "+path)
+
+
+
+def remove_file(path):
+
+	# removing the file
+	if not os.remove(path):
+
+		# success message
+		print(f"{path} is removed successfully")
+
+	else:
+
+		# failure message
+		print("Unable to delete the "+path)
+
+
+def get_file_or_folder_age(path):
+
+	# getting ctime of the file/folder
+	# time will be in seconds
+	ctime = os.stat(path).st_ctime
+
+	# returning the time
+	return ctime
+
+if __name__ == '__main__':
+	main()
